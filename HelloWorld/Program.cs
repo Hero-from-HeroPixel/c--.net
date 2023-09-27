526acc5e79aad6b1a26dd24b676812802db90929
Jean-Jacques Grobler <groblersales@gmail.com>
2023-09-27 21:47:15 +0200

    arrays

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] myStringArray = new string[1];
+
+myStringArray[0] = "Guac";
+Console.WriteLine(myStringArray[0]);
+Console.WriteLine(myStringArray[1]);
+
+Dictionary<string, string[]> myGroceryDictionary = new Dictionary<string, string[]>{
+    {"Dairy" , new string[]{"Cheese", "Milk"}}
+};
