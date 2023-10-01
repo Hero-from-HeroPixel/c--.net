aa4670f3eb25b1e4a03c79cddc6f72ee838848e5
Jean-Jacques Grobler <groblersales@gmail.com>
2023-10-01 13:35:59 +0200

    feat: switch to Entity Framework

--- DotNetApi/Controllers/UserController.cs
+++ DotNetApi/Controllers/UserController.cs
@@ -10,98 +10,169 @@ namespace DotNetApi.Controllers;
 public class UserController : ControllerBase
 {
 
-    DataContextDapper _dapper;
+    // DataContextDapper _dapper;
+    DataContextEF _entityFramework;
     public UserController(IConfiguration config)
     {
-        _dapper = new DataContextDapper(config);
+        // _dapper = new DataContextDapper(config);
+        _entityFramework = new DataContextEF(config);
     }
 
-    [HttpGet("TestConnection")]
-    public DateTime TestConnection()
-    {
-        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
-    }
+
+    // [HttpGet("TestConnection")]
+    // public DateTime TestConnection()
+    // {
+    //     return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    // }
 
     [HttpGet("GetUsers")]
     // public IEnumerable<User> GetUsers()
     public IEnumerable<User> GetUsers()
     {
-        string sql = @"
-        SELECT 
-            [UserId],
-            [FirstName],
-            [LastName],
-            [Email],
-            [Gender],
-            [Active] 
-        FROM TutorialAppSchema.Users 
-        ";
-        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        IEnumerable<User> users = _entityFramework.Users.ToList<User>();
+        // string sql = @"
+        // SELECT 
+        //     [UserId],
+        //     [FirstName],
+        //     [LastName],
+        //     [Email],
+        //     [Gender],
+        //     [Active] 
+        // FROM TutorialAppSchema.Users 
+        // ";
+        // IEnumerable<User> users = _dapper.LoadData<User>(sql);
         return users;
 
     }
     [HttpGet("GetSingleUser/{userId}")]
-    public User GetSingleUser(string userId)
+    public User GetSingleUser(int userId)
     {
-        string sql = @"
-        SELECT 
-            [UserId],
-            [FirstName],
-            [LastName],
-            [Email],
-            [Gender],
-            [Active] 
-        FROM TutorialAppSchema.Users 
-        WHERE UserId = " + userId.ToString()
-        ;
-        User user = _dapper.LoadDataSingle<User>(sql);
-        return user;
+        User? user = _entityFramework.Users.Where<User>(u => u.UserId == userId)
+        .FirstOrDefault<User>();
+        // string sql = @"
+        // SELECT 
+        //     [UserId],
+        //     [FirstName],
+        //     [LastName],
+        //     [Email],
+        //     [Gender],
+        //     [Active] 
+        // FROM TutorialAppSchema.Users 
+        // WHERE UserId = " + userId.ToString()
+        // ;
+        // User user = _dapper.LoadDataSingle<User>(sql);
+        if (user != null)
+        {
+            return user;
+        }
+
+        throw new Exception("No Users found");
     }
 
     [HttpPut("EditUser")]
     public IActionResult EditUser(User user)
     {
-        string sql = @"
-            UPDATE TutorialAppSchema.Users 
-                SET [FirstName] = '" + user.FirstName +
-                "',[LastName]= '" + user.LastName +
-                "',[Email]= '" + user.Email +
-                "',[Gender]= '" + user.Gender +
-                "',[Active]= '" + user.Active +
-            "' WHERE UserId = " + user.UserId;
-        if (_dapper.ExecuteSql(sql))
-        {
-            return Ok();
-        }
+        // string sql = @"
+        //     UPDATE TutorialAppSchema.Users 
+        //         SET [FirstName] = '" + user.FirstName +
+        //         "',[LastName]= '" + user.LastName +
+        //         "',[Email]= '" + user.Email +
+        //         "',[Gender]= '" + user.Gender +
+        //         "',[Active]= '" + user.Active +
+        //     "' WHERE UserId = " + user.UserId;
+        // if (_dapper.ExecuteSql(sql))
+        // {
+        //     return Ok();
+        // }
 
-        throw new Exception("Failed to update user");
+        User? userDb = _entityFramework.Users.Where<User>(u => u.UserId == user.UserId)
+            .FirstOrDefault<User>();
+        // string sql = @"
+        // SELECT 
+        //     [UserId],
+        //     [FirstName],
+        //     [LastName],
+        //     [Email],
+        //     [Gender],
+        //     [Active] 
+        // FROM TutorialAppSchema.Users 
+        // WHERE UserId = " + userId.ToString()
+        // ;
+        // User user = _dapper.LoadDataSingle<User>(sql);
+        if (userDb != null)
+        {
+            userDb.Active = user.Active;
+            userDb.FirstName = user.FirstName;
+            userDb.LastName = user.LastName;
+            userDb.Gender = user.Gender;
+            userDb.Email = user.Email;
+            if (_entityFramework.SaveChanges() > 0)
+            {
+
+                return Ok();
+            }
+            throw new Exception("Failed to update user");
+        }
+        throw new Exception("Failed to get User");
 
     }
 
     [HttpPost("AddUser")]
     public IActionResult AddUser(UserDto user)
     {
-        string sql = @"
-            INSERT INTO TutorialAppSchema.Users (
-                    [FirstName]," +
-                    "[LastName]," +
-                    "[Email]," +
-                    "[Gender]," +
-                    "[Active]" +
-            ") VALUES (" +
-                "'" + user.FirstName + "', " +
-                "'" + user.LastName + "', " +
-                "'" + user.Email + "', " +
-                "'" + user.Gender + "', " +
-                "'" + user.Active + "'" +
-                ")";
+        User userDb = new();
+        userDb.Active = user.Active;
+        userDb.FirstName = user.FirstName;
+        userDb.LastName = user.LastName;
+        userDb.Gender = user.Gender;
+        userDb.Email = user.Email;
+        _entityFramework.Add<User>(userDb);
 
-        Console.WriteLine(sql);
-        if (_dapper.ExecuteSql(sql))
+        if (_entityFramework.SaveChanges() > 0)
         {
+
             return Ok();
         }
+        throw new Exception("Failed to Add user");
+        // string sql = @"
+        //     INSERT INTO TutorialAppSchema.Users (
+        //             [FirstName]," +
+        //             "[LastName]," +
+        //             "[Email]," +
+        //             "[Gender]," +
+        //             "[Active]" +
+        //     ") VALUES (" +
+        //         "'" + user.FirstName + "', " +
+        //         "'" + user.LastName + "', " +
+        //         "'" + user.Email + "', " +
+        //         "'" + user.Gender + "', " +
+        //         "'" + user.Active + "'" +
+        //         ")";
 
-        throw new Exception("Failed to add user");
+        // Console.WriteLine(sql);
+        // if (_dapper.ExecuteSql(sql))
+        // {
+        //     return Ok();
+        // }
+    }
+
+    [HttpDelete()]
+    public IActionResult DeleteUser(int userId)
+    {
+        User? userDb = _entityFramework.Users.Where<User>(u => u.UserId == userId)
+        .FirstOrDefault<User>();
+
+        if (userDb != null)
+        {
+            _entityFramework.Users.Remove(userDb);
+            // string sql = "DELETE FROM TutorialAppSchema.Users WHERE UserId = " + userId.ToString();
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to delete user");
+        }
+        throw new Exception("No User found");
     }
 }
