07d3e6a3ae5616ca04964f9ee0732965df84bb78
Jean-Jacques Grobler <groblersales@gmail.com>
2023-10-03 15:57:52 +0200

    feat: post UPSERT

---
+++ DotNetApi/Stored Procedures/spPosts_Upsert.sql
@@ -0,0 +1,44 @@
+USE DotNetCourseDatabase
+GO
+
+
+-- EXECUTE TutorialAppSchema.spPosts_Get @PostId = 2, @UserId = 1003 
+-- EXECUTE TutorialAppSchema.spPosts_Get @UserId = 1003, @SearchParam = 'second' 
+-- EXECUTE TutorialAppSchema.spPosts_Get @PostId = 2
+-- EXECUTE TutorialAppSchema.spPosts_Get
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPosts_Upsert
+    @UserId INT,
+    @PostTitle NVARCHAR(255),
+    @PostContent NVARCHAR(MAX),
+    @PostId INT = NULL 
+
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM TutorialAppSchema.Posts WHERE PostId = @PostId )
+        BEGIN
+            INSERT INTO TutorialAppSchema.Posts(
+                [UserId],
+                [PostTitle],
+                [PostContent],
+                [PostCreated],
+                [PostUpdated]
+            ) VALUES (
+                @UserId,
+                @PostTitle,
+                @PostContent,
+                GETDATE(),
+                GETDATE()
+            )
+        END
+        ELSE
+        BEGIN
+            UPDATE TutorialAppSchema.Posts
+                SET 
+                PostTitle = @PostTitle,
+                PostContent = @PostContent,
+                PostUpdated = GETDATE()
+            WHERE PostId = @PostId
+
+        END
+
+END
